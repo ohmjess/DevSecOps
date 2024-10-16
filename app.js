af908a0615f303e6475ebc0dc9fc4a793ef84ef0
boytur <104257779+boytur@users.noreply.github.com>
2024-10-16 09:10:45 +0700

    release/0.1

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   // Unused variable
   const unusedVar = "This variable is not used anywhere";
-  res.status(200).send("bom gay");
+  res.status(200).send("Ohmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm");
 });
 
 app.get("/about", (req, res) => {
