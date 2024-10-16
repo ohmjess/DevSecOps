5eb641be95d8a99badecc29012e5061b48f02bb2
boytur <104257779+boytur@users.noreply.github.com>
2024-10-16 09:24:54 +0700

    fix conflic

--- __test__/app.spec.js
+++ __test__/app.spec.js
@@ -2,7 +2,6 @@
 const request = require("supertest");
 const app = require("../app"); // ปรับตามเส้นทางที่ถูกต้อง
 
-
 describe("GET /", () => {
   it("responds with status 200 and returns the expected message", async () => {
     const response = await request(app).get("/");
@@ -17,21 +16,22 @@ describe("GET /about", () => {
     expect(response.status).toBe(200);
     expect(response.text).toContain("about gay"); // เปลี่ยนข้อความให้ตรงกับเนื้อหาใน about.html
   });
-
-describe('GET /', () => {
-    it('responds with status 200 and returns the expected message', async () => {
-        const response = await request(app).get('/');
-        expect(response.status).toBe(200);
-        expect(response.text).toBe("<h1>Hello world! This is our first project towards becoming DevSecOps engineers.</h1>"); // ปรับให้ตรงกับข้อความที่ตอบกลับ
-    });
 });
 
+describe("GET /", () => {
+  it("responds with status 200 and returns the expected message", async () => {
+    const response = await request(app).get("/");
+    expect(response.status).toBe(200);
+    expect(response.text).toBe(
+      "<h1>Hello world! This is our first project towards becoming DevSecOps engineers.</h1>"
+    ); // ปรับให้ตรงกับข้อความที่ตอบกลับ
+  });
+});
 
-
-describe('GET /about', () => {
-    it('responds with status 200 and renders the about page', async () => {
-        const response = await request(app).get('/about');
-        expect(response.status).toBe(200);
-        expect(response.text).toContain('about page'); // เปลี่ยนข้อความให้ตรงกับเนื้อหาใน about.html
-    });
+describe("GET /about", () => {
+  it("responds with status 200 and renders the about page", async () => {
+    const response = await request(app).get("/about");
+    expect(response.status).toBe(200);
+    expect(response.text).toContain("about page"); // เปลี่ยนข้อความให้ตรงกับเนื้อหาใน about.html
+  });
 });
